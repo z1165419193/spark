6d6ba35ec40ebf1bd6017b749620c500517206d4
zyl <1165419193@qq.com>
2018-07-08 14:27:23 +0800

    add test data and add last code and info about zhongyuangongxueyuanlast

---
+++ datasearch/universitesnews/test/hhh.py
@@ -0,0 +1,39 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+
+
+def resapce(word):
+    return word.replace('\n','').replace('\r','').replace('\t','').replace(' ','').replace('\xa0','').replace('&nbsp;','')
+def get_text(url1):
+    html1=urllib.request.urlopen(url1).read().decode('utf-8')
+    soup1=BeautifulSoup(html1,"html.parser")
+    pattern=soup1.find_all( 'span',attrs={'class':'Article_Title'},limit=14)
+    rel = r'href="(.*?)"'
+    idd=21520100
+    for id in pattern:
+        url2='http://www.htu.edu.cn'+re.findall(rel,str(id),re.S)[0]
+        print(url2)
+        html2=urllib.request.urlopen(url2).read().decode('utf-8')
+        soup2=BeautifulSoup(html2)
+        idd=idd+1
+        with open('henanshifandaxue.txt', 'a+', encoding='utf-8') as f:
+                f.write(str(idd)+ '====')
+        title=soup2.find_all('span','Article_Title')
+        for t1 in title:
+            tit=resapce(t1.get_text())
+            with open('henanshifandaxue.txt', 'a+', encoding='utf-8') as f:
+                f.write(tit + '====')
+        content=soup2.find_all('div','Article_Content')
+        for c1 in content:
+            con = resapce(c1.get_text())
+            with open('henanshifandaxue.txt', 'a+', encoding='utf-8') as f:
+                f.write( con+ '\n')
+
+def main(id):
+    url = 'http://www.htu.edu.cn/8956/list'+ str(id)+'.htm'
+    html = get_text(url)
+
+if __name__=='__main__':
+    for i in range(1,48):
+        main(i)

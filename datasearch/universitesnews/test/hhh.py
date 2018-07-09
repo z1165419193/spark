fb11a5ec45fc43db87b5256de55b6d7e31bc7d70
zyl <1165419193@qq.com>
2018-07-09 17:28:51 +0800

    update id mistake

--- datasearch/universitesnews/test/hhh.py
+++ datasearch/universitesnews/test/hhh.py
@@ -5,20 +5,20 @@ import re
 
 def resapce(word):
     return word.replace('\n','').replace('\r','').replace('\t','').replace(' ','').replace('\xa0','').replace('&nbsp;','')
-def get_text(url1):
+def get_text(url1,idd1):
     html1=urllib.request.urlopen(url1).read().decode('utf-8')
     soup1=BeautifulSoup(html1,"html.parser")
     pattern=soup1.find_all( 'span',attrs={'class':'Article_Title'},limit=14)
     rel = r'href="(.*?)"'
-    idd=21520100
+    idd=idd1
     for id in pattern:
+        idd = idd + 1
+        with open('henanshifandaxue.txt', 'a+', encoding='utf-8') as f:
+            f.write(str(idd) + '====')
         url2='http://www.htu.edu.cn'+re.findall(rel,str(id),re.S)[0]
         print(url2)
         html2=urllib.request.urlopen(url2).read().decode('utf-8')
         soup2=BeautifulSoup(html2)
-        idd=idd+1
-        with open('henanshifandaxue.txt', 'a+', encoding='utf-8') as f:
-                f.write(str(idd)+ '====')
         title=soup2.find_all('span','Article_Title')
         for t1 in title:
             tit=resapce(t1.get_text())
@@ -32,7 +32,9 @@ def get_text(url1):
 
 def main(id):
     url = 'http://www.htu.edu.cn/8956/list'+ str(id)+'.htm'
-    html = get_text(url)
+    idd1 = 21520100+14*(id-1)
+    html = get_text(url,idd1)
+
 
 if __name__=='__main__':
     for i in range(1,48):
